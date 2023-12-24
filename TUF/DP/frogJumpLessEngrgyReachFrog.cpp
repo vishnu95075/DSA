a2d8174dbecf60484be5ff8281e1d6cbadcfe2bb
Vishnu Kumar Prajapati <vishnukp95075@gmail.com>
2023-12-24 07:12:19 +0530

    add

--- TUF/DP/frogJumpLessEngrgyReachFrog.cpp
+++ TUF/DP/frogJumpLessEngrgyReachFrog.cpp
@@ -1,18 +1,20 @@
 // Frog Jump Problem
 
+#include <cmath>
 #include <iostream>
-#include<cmath>
 
 using namespace std;
 class Solution {
    public:
-    int frogJumpMinimumEnegryFind(int a[],int n) {
-		if(n==0) return 0;
-		int left = frogJumpMinimumEnegryFind(a,n-1) + abs(a[n]-a[n-1]);
-		if(n>1)
-		int right = frogJumpMinimumEnegryFind(a,n-2)+abs(a[n]+a[n-2]);
+    int frogJumpMinimumEnegryFind(int a[], int n) {
+        if (n == 0)
+            return 0;
+        int right = INT_MAX;
+        int left = frogJumpMinimumEnegryFind(a, n - 1) + abs(a[n] - a[n - 1]);
+        if (n > 1)
+            right = frogJumpMinimumEnegryFind(a, n - 2) + abs(a[n] + a[n - 2]);
 
-		return (min(left,right));
+        return min(left, right);
     }
 };
 
@@ -26,7 +28,7 @@ int main() {
     }
 
     Solution s1;
-    cout << s1.frogJumpMinimumEnegryFind(a,n) << endl;
+    cout << s1.frogJumpMinimumEnegryFind(a, n) << endl;
 
     return 0;
 }
